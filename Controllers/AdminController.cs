a13ef9328b04a55dbc5774b3d31065a9180d545c
t0hyang <kirstenfaye.zulaybar.cics@ust.edu.ph>
2023-11-29 21:00:04 +0800

    - admin homepage - admin inquiries - admin profile - edit admin profile - about

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerPadG4.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminProfile()
+        {
+            return View("AdminProfile");
+        }
+
+        public IActionResult EditAdminProfile()
+        {
+            return View("EditAdminProfile");
+        }
+
+        public IActionResult AdminHomepage()
+        {
+            return View("AdminHomepage");
+        }
+
+        public IActionResult AdminInquiries()
+        {
+            return View("AdminInquiries");
+        }
+
+    }
+}
